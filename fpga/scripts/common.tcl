7a633746f2f6923d55518575b4d99a37411c42b3
Antti Nurmi <antti.nurmi@tuni.fi>
2025-10-10 20:21:32 +0300

    Scripts WIP

---
+++ fpga/scripts/common.tcl
@@ -0,0 +1,10 @@
+proc print_sep {} {
+  puts "######################################################################"
+}
+
+proc print_file {} {
+  print_sep
+  puts "Running file in path:"
+  puts [info script]
+}
+
--- fpga/scripts/run.tcl
+++ fpga/scripts/run.tcl
@@ -1,1 +1,17 @@
-puts "hello"
+# zeroHETI main FPGA implementation script
+# Author(s): Antti Nurmi <antti.nurmi@tuni.fi>
+
+set FPGA_TCL_DIR   $::env(FPGA_TCL_DIR)
+set FPGA_BUILD_DIR $::env(FPGA_BUILD_DIR)
+set PROJECT        $::env(PROJECT)
+
+source ${FPGA_TCL_DIR}/common.tcl
+
+print_file
+
+puts "\nStarting zeroHETI FPGA flow"
+puts "Board: "
+puts "Target Frequency: MHz\n"
+
+create_project ${PROJECT} ${FPGA_BUILD_DIR} -force 
+#-part ${PART_ID}
