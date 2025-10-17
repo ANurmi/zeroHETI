6adc2c78c71a8823579cc762c4711ababc5b736b
Antti Nurmi <antti.nurmi@tuni.fi>
2025-10-17 13:16:54 +0300

    Modify UART divisor

--- examples/smoke_tests/common/uart.h
+++ examples/smoke_tests/common/uart.h
@@ -2,7 +2,7 @@
 void init_uart(uint32_t freq, uint32_t baud){
 
     // TODO: fix compiler ABI
-    uint32_t divisor = 0x20; //freq / (baud << 4);
+    uint32_t divisor = 0x2; //freq / (baud << 4);
 
     write_u8(UART_INTERRUPT_ENABLE, 0x00); // disable uart interrupt
     write_u8(UART_LINE_CONTROL, 0x80);     // Enable DLAB (set baud rate divisor)
