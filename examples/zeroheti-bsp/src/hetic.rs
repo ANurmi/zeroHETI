b524b7ee8bf8f354a0f77fd234eb473afdd80c04
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-10-21 16:24:06 +0300

    sw(bsp,hetic): add set_pol, set_trig

--- examples/zeroheti-bsp/src/hetic.rs
+++ examples/zeroheti-bsp/src/hetic.rs
@@ -1,8 +1,25 @@
 use crate::mmap::hetic::*;
 use crate::mmio;
 
+// Re-export useful riscv-pac traits
+pub use riscv_pac::{HartIdNumber, InterruptNumber, PriorityNumber};
+
 pub struct HeticIrqLine {
     idx: usize,
+}
+
+#[derive(Clone, Copy)]
+pub enum Trig {
+    Edge = 0,
+    Level = 1,
+}
+
+#[derive(Clone, Copy)]
+pub enum Pol {
+    /// Positive
+    Pos = 0,
+    /// Negative
+    Neg = 1,
 }
 
 impl HeticIrqLine {
@@ -25,6 +42,30 @@ impl HeticIrqLine {
     }
     pub fn disable(&mut self) {
         mmio::unmask_u8(HETIC_BASE + LINE_SIZE * self.idx as usize, 0b1u8 << IE_OFS);
+    }
+
+    /// Set trigger type (edge/level)
+    pub fn set_trig(&mut self, trig: Trig) {
+        match trig {
+            Trig::Edge => mmio::unmask_u8(
+                HETIC_BASE + LINE_SIZE * self.idx as usize,
+                0b1u8 << TRIG_OFS,
+            ),
+            Trig::Level => mmio::mask_u8(
+                HETIC_BASE + LINE_SIZE * self.idx as usize,
+                0b1u8 << TRIG_OFS,
+            ),
+        }
+    }
+
+    /// Set polarity (positive/negative)
+    pub fn set_pol(&mut self, pol: Pol) {
+        match pol {
+            Pol::Pos => {
+                mmio::unmask_u8(HETIC_BASE + LINE_SIZE * self.idx as usize, 0b1u8 << POL_OFS)
+            }
+            Pol::Neg => mmio::mask_u8(HETIC_BASE + LINE_SIZE * self.idx as usize, 0b1u8 << POL_OFS),
+        }
     }
 }
 
--- examples/zeroheti-bsp/src/mmap/hetic.rs
+++ examples/zeroheti-bsp/src/mmap/hetic.rs
@@ -5,6 +5,7 @@ pub const LINE_SIZE: usize = 0x2;
 pub const IE_OFS: usize = 0x0;
 pub const IP_OFS: usize = 0x1;
 pub const TRIG_OFS: usize = 0x2;
+pub const POL_OFS: usize = 0x3;
 pub const HETI_OFS: usize = 0x4;
 pub const NEST_OFS: usize = 0x5;
 
