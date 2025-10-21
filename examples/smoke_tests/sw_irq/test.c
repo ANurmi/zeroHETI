9802423423cb557d0d41bc69cf710a219037acf0
Antti Nurmi <antti.nurmi@tuni.fi>
2025-10-21 10:22:41 +0300

    sw_irq test ok

--- examples/smoke_tests/sw_irq/test.c
+++ examples/smoke_tests/sw_irq/test.c
@@ -2,6 +2,7 @@
 #include "mmio.h"
 #include "uart.h"
 #include "hetic.h"
+#include "csr.h"
 
 static uint8_t isr_flag = 1;
 
@@ -9,6 +10,8 @@ int main() {
 
   init_uart(0x0, 0x0);
   print_uart("[UART] sw_irq test\n");
+
+  csr_write(CSR_MTVT, VECTORS_ADDR);
 
   set_ip(3);
   set_ie(3);
@@ -22,6 +25,6 @@ return isr_flag;
 }
 
 void machine_soft_isr(void) {
-  print_uart("[UART] entered succesfully");
+  print_uart("[UART] entered succesfully\n");
   isr_flag = 0;
 }
