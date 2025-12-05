aba395ca9208d756e5245cc67ab42bd3552bd08f
Antti Nurmi <antti.nurmi@tuni.fi>
2025-12-05 14:09:07 +0000

    Add superheti interface

--- rtl/zeroheti_core.sv
+++ rtl/zeroheti_core.sv
@@ -28,7 +28,7 @@ localparam bit [NumSbrPorts-1:0][NumMgrPorts-1:0] Connectivity = '{
 OBI_BUS mgr_bus [NumSbrPorts]();
 OBI_BUS sbr_bus [NumMgrPorts]();
 
-logic irq_heti, irq_ack, irq_valid;
+logic irq_heti, irq_nest, irq_ack, irq_valid;
 logic [Cfg.num_irqs-1:0] core_irq;
 logic     [IrqWidth-1:0] irq_id;
 logic    [PrioWidth-1:0] irq_level;
@@ -42,7 +42,7 @@ obi_hetic #(
   .rst_ni,
   .ext_irqs_i,
   .irq_heti_o (irq_heti),
-  .irq_nest_o (/*not used yet*/),
+  .irq_nest_o (irq_nest),
   .irq_id_o   (irq_id),
   .irq_valid_o(irq_valid),
   .irq_id_i   (irq_id_claim),
@@ -107,43 +107,15 @@ obi_xbar_intf #(
 );
 
 
-`ifndef SYNTHESIS
-ibex_top_tracing #(
-`else
-ibex_top #(
-`endif
-  .PMPEnable        (0),
-  .PMPGranularity   (0),
-  .PMPNumRegions    (4),
-  .MHPMCounterNum   (0),
-  .MHPMCounterWidth (40),
-  .RV32E            (Cfg.rve),
-  .RV32M            (Cfg.mul),
-  .RV32B            (ibex_pkg::RV32BNone),
-  .WritebackStage   (Cfg.wb_stage),
-  .RegFile          (ibex_pkg::RegFileFF),
-  .ICache           (1'b0),
-  .ICacheECC        (1'b0),
-  .ICacheScramble   (1'b0),
-  .BranchPredictor  (1'b0),
-  .SecureIbex       (1'b0),
-  .CLIC             (1'b1),
-  .HardwareStacking (1'b0),
+superheti #(
   .NumInterrupts    (Cfg.num_irqs),
-  .RndCnstLfsrSeed  (ibex_pkg::RndCnstLfsrSeedDefault),
-  .RndCnstLfsrPerm  (ibex_pkg::RndCnstLfsrPermDefault),
-  .DbgTriggerEn     (0),
   .DmHaltAddr       (dm::HaltAddress),
-  .DmExceptionAddr  (dm::ExceptionAddress),
-  .MClicBaseAddr    (zeroheti_pkg::AddrMap.hetic.base),
-  .BranchTargetALU  (Cfg.bt_alu)
-) i_rt_ibex (
+  .DmExceptionAddr  (dm::ExceptionAddress)
+  //.MClicBaseAddr    (zeroheti_pkg::AddrMap.hetic.base)
+) i_superheti (
   .clk_i,
   .rst_ni,
-  .scan_rst_ni            (1'b0),
-  .ram_cfg_i              (10'b0),
   .hart_id_i              (Cfg.hart_id),
-  .test_en_i              (testmode_i),
   .boot_addr_i            (Cfg.boot_addr),
 
   .instr_req_o            (mgr_bus[1].req),
@@ -151,7 +123,6 @@ ibex_top #(
   .instr_gnt_i            (mgr_bus[1].gnt),
   .instr_rvalid_i         (mgr_bus[1].rvalid),
   .instr_rdata_i          (mgr_bus[1].rdata),
-  .instr_rdata_intg_i     (7'b0),
   .instr_err_i            (mgr_bus[1].err),
 
   .data_req_o             (mgr_bus[2].req),
@@ -163,32 +134,19 @@ ibex_top #(
   .data_wdata_o           (mgr_bus[2].wdata),
   .data_rdata_i           (mgr_bus[2].rdata),
   .data_err_i             (mgr_bus[2].err),
-  .data_rdata_intg_i      (7'b0),
-  .data_wdata_intg_o      (),
 
-  .irq_is_pcs_i           (irq_heti),
+  .irq_heti               (irq_heti),
+  .irq_nest               (irq_nest),
   .irq_i                  (core_irq),
   .irq_id_o               (irq_id_claim),
   .irq_ack_o              (irq_ack),
-  .irq_level_i            (8'(irq_level)),
+  .irq_level_i            (irq_level),
   .irq_shv_i              (1'b1),
-  .irq_priv_i             (2'b11),
-
-  .scramble_key_valid_i   (1'b0),
-  .scramble_key_i         (128'b0),
-  .scramble_nonce_i       (64'b0),
-  .scramble_req_o         (),
 
   .debug_req_i            (debug_req),
   .debug_mode_o           (),
-  .crash_dump_o           (),
-  .double_fault_seen_o    (),
-  .fetch_enable_i         (4'b0101),
-  .core_sleep_o           (),
-
-  .alert_minor_o          (),
-  .alert_major_internal_o (),
-  .alert_major_bus_o      ()
+  .fetch_enable_i         (1'b1),
+  .core_sleep_o           ()
 );
 
 assign mgr_bus[1].be    = 4'b0;
