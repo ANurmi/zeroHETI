3bcc7471270e398a01f0f12de2315c11eac3cceb
Antti Nurmi <antti.nurmi@tuni.fi>
2025-12-16 15:56:14 +0200

    Finalize i2c sanity test

--- examples/smoke_tests/common/i2c.h
+++ examples/smoke_tests/common/i2c.h
@@ -2,20 +2,33 @@ uint8_t get_tip(void) {
   return read_u32(I2C_STATUS) & (1u<<1);
 }
 
-void i2c_write_msg(uint8_t pld, uint8_t sta, uint8_t sto) {
-  write_u32(I2C_TX, pld);
+void i2c_set_cmd(uint8_t sta, uint8_t sto, uint8_t we, uint8_t ack, uint8_t ia){
+  uint8_t r = (we == 0);
+  uint8_t w = (we == 1);
   //                 STA    STO      RD      WR     ACK      IA
-  uint32_t cmd = (sta<<7 | sto<<6 | 0u<<5 | 1u<<4 | 1u<<3 | 0u<<0 );
+  uint32_t cmd = (sta<<7 | sto<<6 | r<<5 | w<<4 | ack<<3 | ia<<0 );
   write_u32(I2C_CMD, cmd);
+}
+
+void i2c_send_addr_frame(uint8_t addr, uint8_t we){
+  uint8_t tx_addr = (addr << 1 | we);
+  write_u32(I2C_TX, tx_addr);
+  i2c_set_cmd(1, 0, 1, 0, 0);
   while(get_tip());
 }
 
-void i2c_read_msg(uint8_t sta, uint8_t sto){
-  //                  STA    STO      RD      WR     ACK      IA
-  uint32_t cmd = ( sta<<7 | sto<<6 | 1u<<5 | 0u<<4 | 1u<<3 | 1u<<0 );
-  write_u32(I2C_CMD, cmd);
+void i2c_send_data_frame(uint8_t data, uint8_t last){
+  write_u32(I2C_TX, data);
+  i2c_set_cmd(0, last, 1, 0, 0);
   while(get_tip());
 }
+
+uint8_t i2c_recv_data_frame(uint8_t last){
+  i2c_set_cmd(0, last, 0, 0, 0);
+  while(get_tip());
+  return read_u8(I2C_RX);
+}
+
 void i2c_set_prescaler(uint32_t val){
   write_u32(I2C_CLK_PRESCALER, val);
 }
