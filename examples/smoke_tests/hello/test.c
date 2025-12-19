898a16e95b2995f32bad6f231205d7aff36fe984
Antti Nurmi <antti.nurmi@tuni.fi>
2025-12-19 16:00:33 +0000

    Test Verilator 5.042

--- examples/smoke_tests/hello/test.c
+++ examples/smoke_tests/hello/test.c
@@ -6,7 +6,6 @@ int main() {
 
   init_uart(100000000, 3000000);
 
-  write_u8(0x20051, 0x0);
   print_uart("[UART] Hello from zeroHETI!\n");
   print_uart("[UART] more elaborate prints to exercise the UART\n");
 
