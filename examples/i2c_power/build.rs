e5d26634dcd7839d70bde5ea0e618b432d8cffa9
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2026-01-14 13:00:56 +0200

    fix: make build.rs fail loud

--- examples/i2c_power/build.rs
+++ examples/i2c_power/build.rs
@@ -5,32 +5,32 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     let cargo_flags = env::var("CARGO_ENCODED_RUSTFLAGS").unwrap();
 
-    if let Ok(target) = RiscvTarget::build(&target, &cargo_flags) {
-        // set environment variable RISCV_RT_BASE_ISA to the base ISA of the target.
-        println!(
-            "cargo:rustc-env=RISCV_RT_BASE_ISA={}",
-            target.llvm_base_isa()
-        );
+    let target = RiscvTarget::build(&target, &cargo_flags).unwrap();
 
-        // set environment variable RISCV_RT_LLVM_ARCH_PATCH to patch LLVM bug.
-        // (this env variable is temporary and will be removed after LLVM being fixed)
-        println!(
-            "cargo:rustc-env=RISCV_RT_LLVM_ARCH_PATCH={}",
-            target.llvm_arch_patch()
-        );
+    // set environment variable RISCV_RT_BASE_ISA to the base ISA of the target.
+    println!(
+        "cargo:rustc-env=RISCV_RT_BASE_ISA={}",
+        target.llvm_base_isa()
+    );
 
-        println!(
-            "cargo:rustc-env=RISCV_ISA={}",
-            target
-                .rustc_flags()
-                .into_iter()
-                .map(|s| s.strip_prefix("riscv").unwrap().to_owned())
-                .collect::<String>()
-        );
+    // set environment variable RISCV_RT_LLVM_ARCH_PATCH to patch LLVM bug.
+    // (this env variable is temporary and will be removed after LLVM being fixed)
+    println!(
+        "cargo:rustc-env=RISCV_RT_LLVM_ARCH_PATCH={}",
+        target.llvm_arch_patch()
+    );
 
-        // make sure that these env variables are not changed without notice.
-        println!("cargo:rerun-if-env-changed=RISCV_ISA");
-        println!("cargo:rerun-if-env-changed=RISCV_RT_BASE_ISA");
-        println!("cargo:rerun-if-env-changed=RISCV_RT_LLVM_ARCH_PATCH");
-    }
+    println!(
+        "cargo:rustc-env=RISCV_ISA={}",
+        target
+            .rustc_flags()
+            .into_iter()
+            .map(|s| s.strip_prefix("riscv").unwrap().to_owned())
+            .collect::<String>()
+    );
+
+    // make sure that these env variables are not changed without notice.
+    println!("cargo:rerun-if-env-changed=RISCV_ISA");
+    println!("cargo:rerun-if-env-changed=RISCV_RT_BASE_ISA");
+    println!("cargo:rerun-if-env-changed=RISCV_RT_LLVM_ARCH_PATCH");
 }
