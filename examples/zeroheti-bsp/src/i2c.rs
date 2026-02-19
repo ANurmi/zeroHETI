5f5d8d60b6579d77a8601e2f0108ce05634064fd
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2026-02-19 13:48:21 +0200

    sw: impl tier 2 I2C driver

--- examples/zeroheti-bsp/src/i2c.rs
+++ examples/zeroheti-bsp/src/i2c.rs
@@ -5,6 +5,18 @@ use crate::{
 
 pub struct I2c<const BASE_ADDR: usize>;
 
+/// Write enable for I2C transaction, 0 for read and 1 for write
+#[repr(u8)]
+enum We {
+    Read = 0,
+    Write = 1,
+}
+
+enum Last {
+    NotLast = 0,
+    Last = 1,
+}
+
 impl<const BASE_ADDR: usize> I2c<BASE_ADDR> {
     /// # Parameters
     ///
@@ -65,5 +77,35 @@ impl<const BASE_ADDR: usize> I2c<BASE_ADDR> {
 
     pub fn irq_disable(&mut self) {
         unmask_u8(BASE_ADDR + I2C_CTRL_OFS, 1 << 6);
+    }
+
+    pub fn write_tx(&mut self, addr: u8, buf: &[u8]) {
+        self.send_addr_frame(addr, We::Write as u8);
+
+        for byte in &buf[0..buf.len() - 1] {
+            self.send_data_frame(*byte, Last::NotLast as u8);
+        }
+        self.send_data_frame(buf[buf.len() - 1], Last::Last as u8);
+    }
+
+    /// # Parameters
+    ///
+    /// * `buf` - buffer to store the read data, should have enough space for `read_len` bytes.
+    ///           Existing bytes in the buffer will be overwritten up to
+    ///           read_len.
+    ///
+    /// # Safety
+    ///
+    /// `buf` must have enough space for read_len bytes, and the caller must ensure that the I2C
+    /// transaction is valid (e.g. the slave device at `addr` should be able to provide `read_len`
+    /// bytes of data).
+    pub unsafe fn read_tx(&mut self, addr: u8, buf: &mut [u8], read_len: usize) -> usize {
+        self.send_addr_frame(addr, We::Read as u8);
+
+        for idx in 0..(read_len - 1) {
+            unsafe { *buf.get_unchecked_mut(idx) = self.recv_data_frame(Last::NotLast as u8) };
+        }
+        buf[read_len - 1] = self.recv_data_frame(Last::Last as u8);
+        read_len
     }
 }
