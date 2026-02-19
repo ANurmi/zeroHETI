fe64ad9df89619d6f858657264fe1995c0b4b83d
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2026-02-19 17:19:43 +0200

    sw: scaffold motor control demo

---
+++ examples/motor_control/src/lib.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+
+#[cfg(feature = "intc-clic")]
+use bsp::clic::{Clic, Polarity, Trig};
+use bsp::sprintln;
+use riscv_rt::InterruptNumber;
+
+pub const UART_BAUD: u32 = if cfg!(feature = "rtl-tb") {
+    1_500_000
+} else {
+    115_200
+};
+
+/// Setup `irq` for use with some basic defaults
+///
+/// Copy and customize this function if you need more involved configurations.
+pub fn setup_irq(irq: impl InterruptNumber) {
+    #[cfg(feature = "intc-clic")]
+    {
+        sprintln!("Set up IRQ (id = {})", irq.number());
+        Clic::attr(irq).set_trig(Trig::Edge);
+        Clic::attr(irq).set_polarity(Polarity::Pos);
+        Clic::attr(irq).set_shv(true);
+        Clic::ctl(irq).set_level(0x88);
+        unsafe { Clic::ie(irq).enable() };
+    }
+    #[cfg(not(feature = "intc-clic"))]
+    sprintln!("Interrupt controller not set up. Please set intc-* feature");
+}
+
+/// Tear down the IRQ configuration to avoid side-effects for further testing
+///
+/// Copy and customize this function if you need more involved configurations.
+pub fn tear_irq(irq: impl InterruptNumber) {
+    #[cfg(feature = "intc-clic")]
+    {
+        sprintln!("Tear down (id = {})", irq.number());
+        Clic::ie(irq).disable();
+        Clic::ctl(irq).set_level(0x0);
+        Clic::attr(irq).set_shv(false);
+        Clic::attr(irq).set_trig(Trig::Level);
+        Clic::attr(irq).set_polarity(Polarity::Pos);
+    }
+    #[cfg(not(feature = "intc-clic"))]
+    sprintln!("Interrupt controller not set up. Please set intc-* feature");
+}
+
+/// Print the name of the current file, i.e., test name.
+///
+/// This must be a macro to make sure core::file matches the file this is
+/// invoked in.
+#[macro_export]
+macro_rules! print_example_name {
+    () => {
+        use bsp::sprintln;
+        sprintln!("[{}]", core::file!());
+    };
+}
+
+#[macro_export]
+macro_rules! print_reg_u32 {
+    ($reg:expr) => {
+        use bsp::read_u32;
+        sprintln!("{:#x}: {} \"{}\"", $reg, read_u32($reg), stringify!($reg));
+    };
+}
+
+#[macro_export]
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            core::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        name.strip_suffix("::f").unwrap()
+    }};
+}
---
+++ examples/motor_control/src/main.rs
@@ -0,0 +1,73 @@
+#![no_main]
+#![no_std]
+#![allow(static_mut_refs)]
+#![allow(non_snake_case)]
+
+use bsp::{
+    CPU_FREQ_HZ,
+    apb_uart::*,
+    i2c::I2c,
+    interrupt::{CoreInterrupt, ExternalInterrupt},
+    mmap::i2c::*,
+    mmio::write_u32,
+    mtimer::*,
+    nested_interrupt,
+    register::mintstatus::Mintstatus,
+    riscv::{self},
+    rt::entry,
+    sprintln,
+    tb::signal_pass,
+};
+use fugit::ExtU64;
+use motor_control::*;
+
+struct SimParams {
+    hyperperiod_ms: u64,
+}
+
+const SIM_PARAMS: SimParams = SimParams { hyperperiod_ms: 1 };
+
+// Global variables
+static mut I2C: Option<I2c<I2C_BASE>> = None;
+
+#[entry]
+fn main() -> ! {
+    let _serial = ApbUart::init(CPU_FREQ_HZ, 115_200);
+    let riscv_isa = core::env!("RISCV_ISA");
+    sprintln!("[Motor control demo] ISA = {riscv_isa}");
+
+    let mut i2c = I2c::<I2C_BASE>::init(4);
+
+    unsafe { I2C.replace(i2c) };
+
+    // HACK: clear mintstatus
+    unsafe { bsp::register::mintstatus::write(0.into()) };
+
+    // Set level bits to 8
+    #[cfg(feature = "intc-clic")]
+    bsp::clic::Clic::smclicconfig().set_mnlbits(8);
+    setup_irq(CoreInterrupt::MachineTimer);
+
+    // Start the simulation
+    let mut mtimer = MTimer::instance().into_oneshot();
+    //mtimer.start(SIM_PARAMS.hyperperiod_ms.millis());
+    mtimer.start(10.micros());
+    unsafe {
+        // clear instruction & cycle counters
+        riscv::register::minstret::write(0);
+        riscv::register::mcycle::write(0);
+        riscv::register::minstreth::write(0);
+        riscv::register::mcycleh::write(0);
+        // Global enable
+        riscv::interrupt::enable();
+    }
+
+    loop {
+        //wfi();
+    }
+}
+
+#[bsp::core_interrupt(CoreInterrupt::MachineTimer)]
+unsafe fn MachineTimer() {
+    signal_pass(None);
+}
