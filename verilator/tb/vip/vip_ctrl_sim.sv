57507673780168b736f3d66911a4b5d7e5ee7c90
Antti Nurmi <antti.nurmi@tuni.fi>
2026-02-26 16:40:21 +0200

    Clean up sporadic tasks

--- verilator/tb/vip/vip_ctrl_sim.sv
+++ verilator/tb/vip/vip_ctrl_sim.sv
@@ -28,9 +28,9 @@ module vip_ctrl_sim #(
   localparam longint unsigned RepOfsUs = 'd1_000;
   localparam longint unsigned MbxOfsUs = 'd2_000;
 
-  localparam longint unsigned MbxDlUs = 'd5_000;
-  localparam longint unsigned WrnDlUs = 'd4_000;
-  localparam longint unsigned RepDlUs = 'd3_000;
+  localparam longint unsigned MbxDlUs = 'd3_000;
+  localparam longint unsigned WrnDlUs = 'd2_000;
+  localparam longint unsigned RepDlUs = 'd1_500;
 
   logic [3:0][31:0] voltages = 0;
   logic [3:0][15:0] tune     = 0;
