ec1c835606318af2f6477bc7b5b0ecc66b554e20
krabch <abdelkadir.chantar@tuni.fi>
2026-03-09 10:50:41 +0200

    Update zephyr based CLIC test

--- examples/zephyr/clic/src/main.c
+++ examples/zephyr/clic/src/main.c
@@ -10,8 +10,6 @@
 #include <debug/debug.h>
 #include <zephyr/drivers/interrupt_controller/riscv_clic.h>
 #include </home/abdelkadir/workspace_dir/zephyr/drivers/interrupt_controller/intc_clic.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
 
 #if DT_HAS_COMPAT_STATUS_OKAY(riscv_clic)
 #define CLIC_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(riscv_clic)
@@ -20,54 +18,58 @@
 #endif
 
 #define IRQN  26
+#define CLIC_BASE_ADDR  DT_REG_ADDR(CLIC_NODE)
+#define CLIC_IP(IRQN)   (CLIC_BASE_ADDR + 0x1000 + (IRQN) * 4)
+#define CLIC_IE(IRQN)   (CLIC_BASE_ADDR + 0x1001 + (IRQN) * 4)
+#define CLIC_ATTR(IRQN) (CLIC_BASE_ADDR + 0x1002 + (IRQN) * 4)
+#define CLIC_CTL(IRQN)  (CLIC_BASE_ADDR + 0x1003 + (IRQN) * 4)
+
+static volatile bool interrupt_fired = false;
 
 void riscv_clic_irq_set_pending(uint32_t irq);
-static void clic_isr(const void *arg)
+static void clic_isr(void)
 {
-    volatile uint32_t *hits = (volatile uint32_t *)arg;
-    (*hits)++;
+    interrupt_fired = true;
 }
 
 int main(void)
 {
     printf("CLIC Demonstrator on %s\n", CONFIG_BOARD_TARGET);
 	
-	static volatile uint32_t isr_hits = 0;
 	uint32_t halt 	  =1;	
-	uintptr_t base = DT_REG_ADDR(CLIC_NODE);
-	uintptr_t ip   = base + 0x1000 + IRQN*4;
-	uintptr_t ie   = base + 0x1001 + IRQN*4;
-	uintptr_t attr = base + 0x1002 + IRQN*4;
-	uintptr_t ctl  = base + 0x1003 + IRQN*4;
 
-	csr_write(0x347,0x0);
-    IRQ_CONNECT(IRQN, 10, clic_isr, &isr_hits, 0);
-	
-	riscv_clic_irq_priority_set(IRQN, 10, 1);
+    IRQ_CONNECT(IRQN, 0xFF, clic_isr, NULL, 0);
+	riscv_clic_irq_priority_set(IRQN, 0xFF, 1);
 	riscv_clic_irq_vector_set(IRQN);
+
+	//Force place the interrupt handler address into the mtvt table
+	uint32_t *irq_vector_table = (uint32_t *)(0x10100);
+	irq_vector_table[IRQN] = (uint32_t)clic_isr;
+
     riscv_clic_irq_enable(IRQN);
-	
-	printf("enabled? %d\n", riscv_clic_irq_is_enabled(IRQN));
 
 	printf("ip=%02x ie=%02x attr=%02x ctl=%02x\n",
-			sys_read8(ip), sys_read8(ie), sys_read8(attr), sys_read8(ctl));
+		sys_read8(CLIC_IP(IRQN)), sys_read8(CLIC_IE(IRQN)), 
+		sys_read8(CLIC_ATTR(IRQN)), sys_read8(CLIC_CTRL(IRQN)));
 
 	riscv_clic_irq_set_pending(IRQN);
 
-	printf("after set_pending: ip=%02x ie=%02x attr=%02x ctl=%02x\n",
-	       sys_read8(ip), sys_read8(ie), sys_read8(attr), sys_read8(ctl));
-	
-	
-	//k_busy_wait(10);
-	for(int i=0; i < 200; i++){
+	printf("set_pending: ip=%02x ie=%02x attr=%02x ctl=%02x\n",
+	       sys_read8(CLIC_IP(IRQN)), sys_read8(CLIC_IE(IRQN)), 
+		   sys_read8(CLIC_ATTR(IRQN)), sys_read8(CLIC_CTRL(IRQN)));
+
+	//currently causes hang
+	//k_busy_wait(10); 
+	for(volatile int  i=0; i < 200; i++){
 		halt+=i;
 	}
 
-    if (isr_hits) {
-        printf("[CLIC] PASS hits=%u\n", (unsigned)isr_hits);
+    if (interrupt_fired) {
+        printf("[CLIC] PASS\n");
     } else {
         printf("[CLIC] FAIL no ISR\n");
     }
+	
   	debug_signal_pass();
 
     return 0;
