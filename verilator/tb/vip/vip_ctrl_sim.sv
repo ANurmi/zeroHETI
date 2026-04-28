d43d2c3bbcc19dfcb2900741b5136b3a6e356ccd
krabch <abdelkadir.chantar@tuni.fi>
2026-04-28 13:17:35 +0300

    use tb VIPs from a later commit

--- verilator/tb/vip/vip_ctrl_sim.sv
+++ verilator/tb/vip/vip_ctrl_sim.sv
@@ -7,7 +7,7 @@ module vip_ctrl_sim #(
     output logic [NrIrqs-1:0] irq_o
 );
 
-  /* Internal address mapping 
+/* Internal address mapping
 * 0: 31'h0, sim_en
 * 1: reserved
 * 2: M0 status
@@ -22,15 +22,20 @@ module vip_ctrl_sim #(
 
   localparam int unsigned TaskSetSize = 9;
 
-  localparam longint unsigned MbxPerUs = 'd800;
-  localparam longint unsigned RepPerUs = 'd600;
+  localparam longint unsigned MbxPerUs = 'd5_000;
+  localparam longint unsigned RepPerUs = 'd4_000;
 
-  localparam longint unsigned MbxDlUs = 'd100;
-  localparam longint unsigned WrnDlUs = 'd20;
-  localparam longint unsigned RepDlUs = 'd50;
+  localparam longint unsigned RepOfsUs = 'd1_000;
+  localparam longint unsigned MbxOfsUs = 'd2_000;
+
+  localparam longint unsigned MbxDlUs = 'd3_000;
+  localparam longint unsigned WrnDlUs = 'd2_000;
+  localparam longint unsigned RepDlUs = 'd1_500;
 
   logic [3:0][31:0] voltages = 0;
-  logic [3:0][31:0] speeds   = 0;
+  logic [3:0][15:0] tune = 0;
+  logic [3:0]       tune_vld = 0;
+  logic [3:0][31:0] speeds = 0;
 
   typedef enum logic [1:0] {
     MBX = 0,
@@ -44,7 +49,14 @@ module vip_ctrl_sim #(
     logic [63:0] dl_us;
   } task_t;
 
-  task_t [TaskSetSize-1:0] task_set;
+  typedef struct packed {
+    int unsigned count;
+    longint unsigned slack_worst;
+    longint unsigned slack_avg;
+  } task_ret_t;
+
+  task_t     [TaskSetSize-1:0] task_set;
+  task_ret_t [TaskSetSize-1:0] tasks_ret;
 
   initial begin
     for (int i = 0; i < TaskSetSize; i++) begin
@@ -71,9 +83,9 @@ module vip_ctrl_sim #(
   logic [63:0] time_us = 0;
   logic [7:0] prescaler = 0;
 
-  // Assume 100 MHz sim clock frequency
+  // Assume 10 MHz sim clock frequency
   always @(posedge clk_i) begin : g_us_counter
-    if (prescaler == 'd99) begin
+    if (prescaler == 'd9) begin
       prescaler = 0;
       time_us++;
     end else prescaler++;
@@ -81,41 +93,73 @@ module vip_ctrl_sim #(
 
 
   always @(posedge enable) begin
+    time_us = 0;  // clear vip time when first enabled
+
+    for (int i = 0; i < TaskSetSize; i++) begin
+      tasks_ret[i] = '{default: 0};
+    end
+
     $display("[CTRL_SIM] Starting simulation for task set:");
     $display("[CTRL_SIM] (Periodic): Receive MBX directive,          DL: %3d us", MbxDlUs);
     $display("[CTRL_SIM] (Sporadic): Motor [0-3] speed warning,      DL: %3d us", WrnDlUs);
     $display("[CTRL_SIM] (Periodic): Report M[0-3] speed, timestamp, DL: %3d us", RepDlUs);
   end
 
+  always @(negedge enable) begin
+    $display("[CTRL_SIM] Simulation terminated");
+    for (int i = 0; i < TaskSetSize; i++) begin
+      $display("Task %0d - count: %3d, avg. slack: %4d us, worst slack: %4d us", i,
+               tasks_ret[i].count, tasks_ret[i].slack_avg, tasks_ret[i].slack_worst);
+    end
+  end
+
   always @(time_us) begin : g_dl_counter
+
+    tune_vld = 0;
 
     // Decrement deadlines of active task
     for (int i = 0; i < TaskSetSize; i++) begin
-      if (task_set[i].active) begin
-        if (task_set[i].dl_us == 0) $fatal(1, "Deadline miss for task idx %0d!", i);
-        else task_set[i].dl_us--;
+      if (enable) begin
+        if (task_set[i].active) begin
+          if (task_set[i].dl_us == 0) $fatal(1, "Deadline miss for task idx %0d!", i);
+          else task_set[i].dl_us--;
+        end
       end
     end
 
     // Activate periodic directive task
-    if (enable & (time_us % MbxPerUs == 0)) begin
+    if (enable & ((time_us - MbxOfsUs) % MbxPerUs == 0)) begin
       pend_task(0);
       reset_task_dl(0);
       i_zeroheti.i_mbx.i_sim_mbx.raise_irq();
     end
 
-    // Activate periodic reporting tasks
-    if (enable & (time_us % RepPerUs == 0)) begin
-      pend_task(5);
-      reset_task_dl(5);
-      pend_task(6);
-      reset_task_dl(6);
-      pend_task(7);
-      reset_task_dl(7);
-      pend_task(8);
-      reset_task_dl(8);
+    // Activate periodic reporting tasks with appropriate offset
+    for (int i = 0; i < 4; i++) begin
+      if (enable & ((time_us - RepOfsUs * i) % RepPerUs == 0)) begin
+        pend_task(5 + i);
+        reset_task_dl(5 + i);
+      end
     end
 
+    // Dectivate warning tasks
+    for (int i = 0; i < NrMotors; i++) begin
+      automatic int SporIdx = i + 1;
+      if (~irq_o[i] & task_set[SporIdx].active) begin
+        ack_task(SporIdx);
+      end
+    end
+
+  end : g_dl_counter
+
+  for (genvar i = 0; i < NrMotors; i++) begin : g_sporadic_irqs
+    localparam int SporIdx = i + 1;
+    always @(posedge irq_o[i]) begin
+      if (enable) begin
+        pend_task(SporIdx);
+        reset_task_dl(SporIdx);
+      end
+    end
   end
 
   for (genvar i = 0; i < NrMotors; i++) begin : g_motors
@@ -124,34 +168,89 @@ module vip_ctrl_sim #(
     ) i_motor (
         .clk_i,
         .rst_ni,
-        .enable_i (enable),
-        .voltage_i(voltages[i]),
-        .speed_o  (speeds[i]),
-        .irq_o    (irq_o[i])
+        .enable_i  (enable),
+        .voltage_i (voltages[i]),
+        .tune_i    (tune[i]),
+        .tune_vld_i(tune_vld[i]),
+        .speed_o   (speeds[i]),
+        .irq_o     (irq_o[i])
     );
   end
 
   task write(input logic [6:0] addr, input logic [31:0] data);
-    $display("[CTRL_SIM] write - addr: %h, data: %h", addr, data);
+    automatic string addr_name = "";
     unique case (addr)
-      7'd0: enable    = data[0];
-      7'd3: voltages[0] = data;
-      7'd5: voltages[1] = data;
-      7'd7: voltages[2] = data;
-      7'd9: voltages[3] = data;
+      7'd0: begin
+        enable    = data[0];
+        addr_name = "SimCtrl";
+      end
+      7'd2: begin
+        voltages[0] = data;
+        tune[0]     = 0;
+        addr_name   = "M0_Ctrl";
+      end
+      7'd3: begin
+        tune[0] = data[15:0];
+        tune_vld[0] = 1'b1;
+        addr_name = "M0_Tune";
+      end
+      7'd5: begin
+        voltages[1] = data;
+        tune[1]     = 0;
+        addr_name   = "M1_Ctrl";
+      end
+      7'd6: begin
+        tune[1] = data[15:0];
+        tune_vld[1] = 1'b1;
+        addr_name = "M1_Tune";
+      end
+      7'd8: begin
+        voltages[2] = data;
+        tune[2]     = 0;
+        addr_name   = "M2_Ctrl";
+      end
+      7'd9: begin
+        tune[2] = data[15:0];
+        tune_vld[2] = 1'b1;
+        addr_name = "M2_Tune";
+      end
+      7'd11: begin
+        voltages[3] = data;
+        tune[3]     = 0;
+        addr_name   = "M3_Ctrl";
+      end
+      7'd12: begin
+        tune[3] = data[15:0];
+        tune_vld[3] = 1'b1;
+        addr_name = "M3_Tune";
+      end
       default: ;
     endcase
+    $display("[CTRL_SIM] write - addr: %s, data: 0x%h", addr_name, data);
   endtask
 
   task read(input logic [6:0] addr, output logic [31:0] data);
+    automatic string addr_name = "";
     unique case (addr)
-      7'd2: data = speeds[0];
-      7'd4: data = speeds[1];
-      7'd6: data = speeds[2];
-      7'd8: data = speeds[3];
+      7'd1: begin
+        data = speeds[0];
+        addr_name = "M0_Stat";
+      end
+      7'd4: begin
+        data = speeds[1];
+        addr_name = "M1_Stat";
+      end
+      7'd7: begin
+        data = speeds[2];
+        addr_name = "M2_Stat";
+      end
+      7'd10: begin
+        data = speeds[3];
+        addr_name = "M3_Stat";
+      end
       default: ;
     endcase
-    $display("[CTRL_SIM] read - addr: %h, data: %h", addr, data);
+    $display("[CTRL_SIM] read  - addr: %s, data: 0x%h", addr_name, data);
   endtask
 
   task pend_task(input int unsigned i);
@@ -160,7 +259,31 @@ module vip_ctrl_sim #(
 
   task ack_task(input int unsigned i);
     task_set[i].active = 1'b0;
+    log_slack(i);
   endtask
+
+  task log_slack(input int unsigned i);
+    if (tasks_ret[i].count == 0) begin  // initial state
+      tasks_ret[i].slack_worst = task_set[i].dl_us;
+      tasks_ret[i].slack_avg   = task_set[i].dl_us;
+    end else begin
+      // update worst slack
+      if (tasks_ret[i].slack_worst > task_set[i].dl_us) begin
+        tasks_ret[i].slack_worst = task_set[i].dl_us;
+      end
+
+      // verilator lint_off WIDTHEXPAND
+      // update average slack
+      tasks_ret[i].slack_avg = ((tasks_ret[i].slack_avg * tasks_ret[i].count) + task_set[i].dl_us) 
+                                  / (tasks_ret[i].count + 32'd1);
+      // verilator lint_on WIDTHEXPAND
+
+    end
+
+    tasks_ret[i].count += 1;
+  endtask
+
+
 
   task reset_task_dl(input int unsigned i);
     unique case (i) inside
