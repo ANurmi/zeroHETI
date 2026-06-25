946be2ae8d9c9bb3dd6cf1bc8b854801173cfc7f
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2026-06-25 16:05:08 +0300

    refactor: structure mailbox memory map into regblocks

--- examples/zeroheti-bsp/src/mmap/mailbox.rs
+++ examples/zeroheti-bsp/src/mmap/mailbox.rs
@@ -1,13 +1,35 @@
 pub const MBX_ADDR: usize = 0x3_0000;
 
-pub const INBOX_OFS: usize = 0x00;
-/// HW raises mailbox IRQ, when there is mail
-pub const IRQ_ACK_OFS: usize = 0x04;
-pub const TIME_LO_OFS: usize = 0x08;
-pub const TIME_HI_OFS: usize = 0x0C;
-pub const STAT_M0_OFS: usize = 0x10;
-/*
-pub const STAT_M1_OFS: usize = 0x14;
-pub const STAT_M2_OFS: usize = 0x18;
-pub const STAT_M3_OFS: usize = 0x1C;
-*/
+/// Register block for mailbox
+#[repr(C)]
+pub struct RegisterBlock {
+    /// 0x0..0x4 Status
+    pub stat: Stat,
+    /// 0x4..0x8 Control
+    pub ctrl: Ctrl,
+    // 4 bytes of padding
+    _pad: u32,
+    /// 0xc..0x14 Inbox
+    pub ibox: IoBox,
+    /// 0x14..0x1c Outbox
+    pub obox: IoBox,
+}
+
+bitflags::bitflags! {
+    pub struct Stat: u32 {
+        const OBOX_FULL = 0b1 << 2;
+    }
+
+    pub struct Ctrl: u32 {
+        const DISPATCH = 0b1 << 0;
+        const IRQ_CLEAR = 0b1 << 17;
+        const POP_INBOX = 0b1 << 24;
+    }
+}
+
+/// Inbox/outbox address layout
+#[repr(C)]
+pub struct IoBox {
+    pub addr: u32,
+    pub data: u32,
+}
