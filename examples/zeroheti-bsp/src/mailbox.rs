3b4f5c103444e6c47f3854ebfbdf05b767c8530f
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2026-06-25 16:05:30 +0300

    feat(bsp): new mailbox HAL

--- examples/zeroheti-bsp/src/mailbox.rs
+++ examples/zeroheti-bsp/src/mailbox.rs
@@ -1,86 +1,108 @@
+use core::ptr::{read_volatile, write_volatile};
+
 use crate::mmap::mailbox::*;
-use crate::mmio::{read_u32, write_u32};
+use crate::mmio::{read_u32p, write_u32p};
 
-#[repr(usize)]
-pub enum MotorIdx {
-    M0 = 0,
-    M1 = 1,
-    M2 = 2,
-    M3 = 3,
-}
-
-pub type Mailbox = MailboxHal<MBX_ADDR>;
-
-pub struct MailboxHal<const BASE_ADDR: usize> {
-    inbox: InboxHal<BASE_ADDR>,
-    outbox: OutboxHal<BASE_ADDR>,
-}
-
-impl<const BASE_ADDR: usize> MailboxHal<BASE_ADDR> {
-    /// Retrieve an instance of the mailbox, comprising both halves: inbox and
+/// Relocatable driver for mailbox
+pub struct Mailbox(*mut RegisterBlock);
+impl Mailbox {
+    /// Retrieve an instance of the mailbox comprising both halves: inbox and
     /// outbox. Call `split` to obtain the functional halves.
+    ///
+    /// # Safety
+    ///
+    /// This is the global instance. Ensure safe sharing.
     #[inline]
-    pub const unsafe fn instance() -> Self {
-        Self {
-            inbox: InboxHal::<BASE_ADDR>::instance(),
-            outbox: OutboxHal::<BASE_ADDR>::instance(),
+    pub const unsafe fn instance<const BASE_ADDR: usize>() -> Self {
+        Self(BASE_ADDR as *mut _)
+    }
+
+    #[inline]
+    pub fn split(self) -> (Inbox, Outbox) {
+        (Inbox(self.0), Outbox(self.0))
+    }
+}
+
+pub struct Inbox(*mut RegisterBlock);
+impl Inbox {
+    #[inline]
+    fn clear_irq(&mut self) {
+        unsafe { write_volatile(&mut (*self.0).ctrl as *mut _, Ctrl::IRQ_CLEAR) };
+    }
+
+    /// Pop letter from inbox
+    #[inline]
+    fn pop_inbox(&mut self) {
+        unsafe { write_volatile(&mut (*self.0).ctrl as *mut _, Ctrl::POP_INBOX) };
+    }
+
+    /// Returns `(addr, data)`
+    #[inline]
+    pub fn recv(&mut self) -> (u32, u32) {
+        self.clear_irq();
+
+        // Read inbox
+        let addr = read_u32p(unsafe { &mut (*self.0).ibox.addr as *mut u32 });
+        let data = read_u32p(unsafe { &mut (*self.0).ibox.data as *mut u32 });
+
+        self.pop_inbox();
+
+        (addr, data)
+    }
+
+    /// Returns `[(addr, data)]`
+    #[inline]
+    pub fn recv_many(&mut self, buf: &mut [(u32, u32)]) {
+        self.clear_irq();
+
+        for (addr, data) in buf.iter_mut() {
+            // Read inbox
+            *addr = read_u32p(unsafe { &mut (*self.0).ibox.addr as *mut u32 });
+            *data = read_u32p(unsafe { &mut (*self.0).ibox.data as *mut u32 });
+            self.pop_inbox();
         }
     }
-
-    #[inline]
-    pub fn split(self) -> (InboxHal<BASE_ADDR>, OutboxHal<BASE_ADDR>) {
-        (self.inbox, self.outbox)
-    }
-
-    #[inline]
-    pub fn merge(
-        inbox: InboxHal<BASE_ADDR>,
-        outbox: OutboxHal<BASE_ADDR>,
-    ) -> MailboxHal<BASE_ADDR> {
-        Self { inbox, outbox }
-    }
 }
 
-pub type Inbox = InboxHal<MBX_ADDR>;
-pub struct InboxHal<const BASE_ADDR: usize>;
-impl<const BASE_ADDR: usize> InboxHal<BASE_ADDR> {
-    /// Use [MailboxHal::instance]
+pub struct Outbox(*mut RegisterBlock);
+impl Outbox {
     #[inline]
-    const fn instance() -> Self {
-        Self {}
+    fn wait_outbox_empty(&self) {
+        while (unsafe { read_volatile::<Stat>(&(*self.0).stat as *const _) })
+            .intersection(Stat::OBOX_FULL)
+            .is_empty()
+        {}
     }
 
-    /// Retrieve motor control instructions from the mailbox
     #[inline]
-    pub fn read_inbox(&self) -> u32 {
-        read_u32(BASE_ADDR + INBOX_OFS)
+    pub fn send(&mut self, addr: u32, data: u32) {
+        // Ensure outbox has free capacity before sending
+        self.wait_outbox_empty();
+
+        // Write letter address and data
+        write_u32p(unsafe { &mut (*self.0).obox.addr as *mut u32 }, addr);
+        write_u32p(unsafe { &mut (*self.0).obox.data as *mut u32 }, data);
+
+        // Dispatch
+        unsafe { write_volatile(&mut (*self.0).ctrl as *mut _, Ctrl::DISPATCH) };
     }
 
-    /// Acknowledge that mailbox IRQ has been handled
+    /// Sends multiple letters in a row. Hardware causes a wait in-between
+    /// letters.
+    ///
+    /// Parameter is (addr, data)
     #[inline]
-    pub fn ack_irq(&mut self) {
-        write_u32(BASE_ADDR + IRQ_ACK_OFS, 1)
-    }
-}
+    pub fn send_many(&mut self, letters: &[(u32, u32)]) {
+        // Ensure mailbox has free capacity before sending
+        self.wait_outbox_empty();
 
-pub type Outbox = OutboxHal<MBX_ADDR>;
-pub struct OutboxHal<const BASE_ADDR: usize>;
-impl<const BASE_ADDR: usize> OutboxHal<BASE_ADDR> {
-    /// Use [MailboxHal::instance]
-    #[inline]
-    const fn instance() -> Self {
-        Self {}
-    }
+        // Write all letters
+        for (addr, data) in letters {
+            write_u32p(unsafe { &mut (*self.0).obox.addr as *mut u32 }, *addr);
+            write_u32p(unsafe { &mut (*self.0).obox.data as *mut u32 }, *data);
 
-    /// Write time stamp, then a single motor status
-    pub fn write_time_and_stat(&mut self, time: u64, stat: u32, motor: MotorIdx) {
-        // Write time
-        let time_lo = (time & 0xFFFF_FFFF) as u32;
-        let time_hi = (time >> 32) as u32;
-        write_u32(BASE_ADDR + TIME_LO_OFS, time_lo);
-        write_u32(BASE_ADDR + TIME_HI_OFS, time_hi);
-
-        // Write stat
-        write_u32(BASE_ADDR + STAT_M0_OFS + 0x4 * motor as usize, stat);
+            // Dispatch
+            unsafe { write_volatile(&mut (*self.0).ctrl as *mut _, Ctrl::DISPATCH) };
+        }
     }
 }
