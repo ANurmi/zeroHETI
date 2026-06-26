a20ac122cebf02fa2189a7c41357c08b1f644abb
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2026-06-26 13:14:25 +0300

    refactor: use core::ptr

--- examples/zeroheti-bsp/src/mmio.rs
+++ examples/zeroheti-bsp/src/mmio.rs
@@ -5,42 +5,41 @@
 //! Use of these methods is inherently unsafe, as hardware can do whatever. The
 //! only way to assert safety is to not depend on this module.
 
-// We trust users of MMIO to use these functions responsibly
-#![allow(clippy::not_unsafe_ptr_arg_deref)]
+use core::ptr;
 
 #[inline(always)]
 pub fn read_u8(addr: usize) -> u8 {
-    // Safety: unaligned reads may fail to produce expected results.
-    unsafe { core::ptr::read_volatile(addr as *const _) }
+    // Safety: `addr` must be a valid MMIO address for a u8 register.
+    unsafe { ptr::read_volatile(addr as *const u8) }
 }
 
 /// Reads the masked bits from the register
 #[inline(always)]
 pub fn read_u8_masked(addr: usize, mask: u8) -> u8 {
-    // Safety: unaligned reads may fail to produce expected results.
-    unsafe { core::ptr::read_volatile(addr as *const u8) & mask }
+    // Safety: `addr` must be a valid MMIO address for a u8 register.
+    unsafe { ptr::read_volatile(addr as *const u8) & mask }
 }
 
 #[inline(always)]
 pub fn write_u8(addr: usize, val: u8) {
-    // Safety: unaligned reads may fail to produce expected results.
-    unsafe { core::ptr::write_volatile(addr as *mut _, val) }
+    // Safety: `addr` must be a valid MMIO address for a u8 register.
+    unsafe { ptr::write_volatile(addr as *mut u8, val) }
 }
 
 #[inline(always)]
 pub fn write_u16(addr: usize, val: u16) {
-    // Safety: unaligned reads may fail to produce expected results.
-    unsafe { core::ptr::write_volatile(addr as *mut _, val) }
+    // Safety: `addr` must be a valid MMIO address for a u16 register.
+    unsafe { ptr::write_volatile(addr as *mut u16, val) }
 }
 
 #[inline(always)]
 pub fn read_u32(addr: usize) -> u32 {
-    unsafe { core::ptr::read_volatile(addr as *const _) }
+    unsafe { ptr::read_volatile(addr as *const _) }
 }
 
 #[inline(always)]
 pub fn read_u32p(ptr: *const u32) -> u32 {
-    unsafe { core::ptr::read_volatile(ptr) }
+    unsafe { ptr::read_volatile(ptr) }
 }
 
 #[inline(always)]
@@ -50,7 +49,7 @@ pub fn write_u32(addr: usize, val: u32) {
 
 #[inline(always)]
 pub fn write_u32p(ptr: *mut u32, val: u32) {
-    unsafe { core::ptr::write_volatile(ptr, val) }
+    unsafe { ptr::write_volatile(ptr, val) }
 }
 
 #[inline(always)]
@@ -67,8 +66,8 @@ pub fn mask_u32(addr: usize, mask: u32) {
 
 #[inline(always)]
 pub fn mask_u32p(ptr: *mut u32, mask: u32) {
-    let r = unsafe { core::ptr::read_volatile(ptr) };
-    unsafe { core::ptr::write_volatile(ptr, r | mask) }
+    let r = unsafe { ptr::read_volatile(ptr) };
+    unsafe { ptr::write_volatile(ptr, r | mask) }
 }
 
 /// Unmasks specified bits from given register
@@ -80,8 +79,8 @@ pub fn unmask_u32(addr: usize, unmask: u32) {
 /// Unmasks specified bits from given register
 #[inline(always)]
 pub fn unmask_u32p(ptr: *mut u32, unmask: u32) {
-    let r = unsafe { core::ptr::read_volatile(ptr) };
-    unsafe { core::ptr::write_volatile(ptr as *mut _, r & !unmask) }
+    let r = unsafe { ptr::read_volatile(ptr) };
+    unsafe { ptr::write_volatile(ptr, r & !unmask) }
 }
 
 #[inline(always)]
@@ -93,15 +92,15 @@ pub fn toggle_u32(addr: usize, toggle_bits: u32) {
 
 #[inline(always)]
 pub fn mask_u8(addr: usize, mask: u8) {
-    // Safety: unaligned reads/writes may fail to produce expected results.
-    let r = unsafe { core::ptr::read_volatile(addr as *const u8) };
-    unsafe { core::ptr::write_volatile(addr as *mut u8, r | mask) }
+    // Safety: `addr` must be a valid MMIO address for a u8 register.
+    let r = unsafe { ptr::read_volatile(addr as *const u8) };
+    unsafe { ptr::write_volatile(addr as *mut u8, r | mask) }
 }
 
 /// Unmasks specified bits from given register
 #[inline(always)]
 pub fn unmask_u8(addr: usize, unmask: u8) {
-    // Safety: unaligned reads/writes may fail to produce expected results.
-    let r = unsafe { core::ptr::read_volatile(addr as *const u8) };
-    unsafe { core::ptr::write_volatile(addr as *mut u8, r & !unmask) }
+    // Safety: `addr` must be a valid MMIO address for a u8 register.
+    let r = unsafe { ptr::read_volatile(addr as *const u8) };
+    unsafe { ptr::write_volatile(addr as *mut u8, r & !unmask) }
 }
