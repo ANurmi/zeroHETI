a7eccfd1202c5cb44f250e576dabf8eac7c09aa1
Antti Nurmi <antti.nurmi@tuni.fi>
2026-08-11 15:54:27 +0300

    Update UART for 100 MHz CPU speed
    Add print
    
    Verilator 5.050 waiver
    
    TODO: debug regs

--- examples/rt_prof/src/main.rs
+++ examples/rt_prof/src/main.rs
@@ -11,12 +11,27 @@ mod app {
         apb_uart::ApbUart,
         asm_delay,
         i2c::{self, I2c},
+        mmap::apb_timer::TIMER0_ADDR,
+        mmio,
         mtimer::{self, *},
+        riscv::{self},
         sprintln,
         tb::signal_pass,
+        timer_group::{Periodic, Timer},
     };
 
     use fugit::{ExtU32, ExtU64};
+
+    fn clear_perf_counters() {
+        unsafe {
+            riscv::register::minstret::write(0);
+            riscv::register::mcycle::write(0);
+            riscv::register::minstreth::write(0);
+            riscv::register::mcycleh::write(0);
+        }
+    }
+
+    const TIMER_PER_US: u32 = 100;
 
     #[shared]
     struct Shared {
@@ -28,8 +43,37 @@ mod app {
         let _serial = ApbUart::init(CPU_FREQ_HZ, 115_200);
         let i2c = I2c::init(4);
 
-        sprintln!("Start");
+        sprintln!("Start, CPU (MHz): {}", CPU_FREQ_HZ / 1_000_000);
+
+        const CFG_BASE_ADDR: usize = 0x0000_3500;
+
+        let rd = mmio::read_u32(CFG_BASE_ADDR);
+
+        mmio::write_u32(CFG_BASE_ADDR + 4, 1);
+        mmio::write_u32(CFG_BASE_ADDR + 8, 2);
+        mmio::write_u32(CFG_BASE_ADDR + 12, 3);
+        mmio::write_u32(CFG_BASE_ADDR + 16, 4);
+        sprintln!("Version: {:X}", rd);
+
         MTimer::instance().into_oneshot().start(1000u64.micros());
+
+        let timers = &mut [
+            Timer::init::<TIMER0_ADDR>().into_periodic(),
+            /*
+                Timer::init::<TIMER1_ADDR>().into_periodic(),
+                Timer::init::<TIMER2_ADDR>().into_periodic(),
+                Timer::init::<TIMER3_ADDR>().into_periodic(),
+            */
+        ];
+
+        timers
+            .iter_mut()
+            .for_each(|t| t.set_period(TIMER_PER_US.micros()));
+
+        timers.iter_mut().for_each(Periodic::start);
+
+        clear_perf_counters();
+
         Shared { i2c }
     }
 
@@ -46,8 +90,19 @@ mod app {
             sprintln!("End");
 
             // Delay to let UART complete
-            asm_delay(100);
+            asm_delay(10_000);
             signal_pass(None);
+        }
+    }
+
+    #[task(binds = Timer0Cmp, priority = 0xAA)]
+    struct Timer0 {}
+    impl RticTask for Timer0 {
+        fn init() -> Self {
+            Self {}
+        }
+        fn exec(&mut self) {
+            sprintln!("T0");
         }
     }
 }
--- rtl/apb_cfg_regs.sv
+++ rtl/apb_cfg_regs.sv
@@ -5,12 +5,53 @@ module apb_cfg_regs #(
     APB.Slave apb_i
 );
 
+  logic [7:0] local_addr;
+  assign local_addr = apb_i.paddr[7:0];
+
   // TODO: add more relevant platform information/configuration
   logic [31:0] short_hash;
-  assign short_hash   = 32'h`GIT_HASH;
+  assign short_hash = 32'h`GIT_HASH;
 
-  assign apb_i.prdata = short_hash;
-  assign apb_i.pready = apb_i.psel & apb_i.penable;
+  logic [3:0][31:0] gpreg_d, gpreg_q;
+
+  logic apb_event, read_event;
+
+  assign apb_event = apb_i.psel & apb_i.penable;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (~rst_ni) begin
+      gpreg_q <= '0;
+    end else begin
+      gpreg_q <= gpreg_d;
+    end
+  end
+
+  always_comb begin
+
+    apb_i.prdata = 0;
+    gpreg_d = gpreg_q;
+
+    if (apb_event) begin
+      if (apb_i.pwrite) begin
+        unique case (local_addr)
+          8'h00:  /*read-only*/;
+          8'h04: gpreg_d[0] = apb_i.pwdata;
+          8'h08: gpreg_d[1] = apb_i.pwdata;
+          8'h0C: gpreg_d[2] = apb_i.pwdata;
+          8'h10: gpreg_d[3] = apb_i.pwdata;
+          default: ;
+        endcase
+      end else begin
+        unique case (local_addr)
+          8'h0: apb_i.prdata = short_hash;
+          default: ;
+        endcase
+      end
+    end
+  end
+
+  //assign apb_i.prdata  = short_hash;
+  assign apb_i.pready  = apb_i.psel & apb_i.penable;
   assign apb_i.pslverr = 1'b0;
 
 endmodule : apb_cfg_regs
