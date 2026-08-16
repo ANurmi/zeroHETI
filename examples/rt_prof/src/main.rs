72969f7c1b1e11b2c37012a6006a594440ac3dbb
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2026-08-16 19:31:37 +0300

    feat(rt_prof): put obs behind -Fflag & document

--- examples/rt_prof/src/main.rs
+++ examples/rt_prof/src/main.rs
@@ -4,9 +4,11 @@
 
 use bsp::rt as _;
 
+#[cfg(feature = "obs")]
 mod obs;
 
-#[rtic::app(device = bsp, obs = crate::obs::Obs, dispatchers = [Timer0Ovf, Timer1Ovf, Timer2Ovf, Timer3Ovf, Ext0, Ext1, Ext2, Ext3])]
+#[cfg_attr(feature = "obs", rtic::app(device = bsp, obs = crate::obs::Obs, dispatchers = [Timer0Ovf, Timer1Ovf, Timer2Ovf, Timer3Ovf, Ext0, Ext1, Ext2, Ext3]))]
+#[cfg_attr(not(feature = "obs"), rtic::app(device = bsp, dispatchers = [Timer0Ovf, Timer1Ovf, Timer2Ovf, Timer3Ovf, Ext0, Ext1, Ext2, Ext3]))]
 mod app {
     const fn to_prio(per: u32) -> u8 {
         // Shifting out lowest 8 bits makes timing granularity
