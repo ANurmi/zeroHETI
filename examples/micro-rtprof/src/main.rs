d88bd94ce2dd75fba192203eda56c00d0e146140
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2026-08-18 17:27:25 +0300

    Refactor both rtprofs (#129)
    * refactor(micro-rtprof): rm unused code, rename Finish
    * refactor(rt-profs): less fully-qualified names
    * refactor(rt-prof): remove deferred SimStart hack

--- examples/micro-rtprof/src/main.rs
+++ examples/micro-rtprof/src/main.rs
@@ -14,9 +14,9 @@ mod app {
         mailbox::Mailbox,
         mmap::apb_timer::{TIMER0_ADDR, TIMER1_ADDR, TIMER2_ADDR},
         mmio,
-        mtimer::{self, *},
+        mtimer::*,
         parse_u32,
-        riscv::{self},
+        register::{mcycle, mcycleh, minstret, minstreth},
         sprintln,
         tb::signal_pass,
         timer_group::{Periodic, Timer},
@@ -29,10 +29,10 @@ mod app {
 
     fn clear_perf_counters() {
         unsafe {
-            riscv::register::minstret::write(0);
-            riscv::register::mcycle::write(0);
-            riscv::register::minstreth::write(0);
-            riscv::register::mcycleh::write(0);
+            minstret::write(0);
+            mcycle::write(0);
+            minstreth::write(0);
+            mcycleh::write(0);
         }
     }
 
@@ -101,7 +101,7 @@ mod app {
         obx.send(task_dl_base + 1, TIMER1_PER_US * US_TO_CC);
         obx.send(task_dl_base + 2, TIMER2_PER_US * US_TO_CC);
 
-        // TODO: fix API
+        // Setup mtimer to trigger `Finish` task
         MTimer::with_clkdiv(250).into_oneshot().start(RT.millis());
 
         let timers = &mut [
@@ -117,28 +117,25 @@ mod app {
         timers.iter_mut().for_each(Periodic::start);
 
         clear_perf_counters();
-        // scb enable
+        // Scoreboard enable
         mmio::write_u32(CFG_BASE_ADDR + CFG_TASK_OFFS, 1);
 
         Shared { i2c }
     }
 
     #[task(binds = MachineTimer, priority = 0xff)]
-    struct StartSim {
-        mtimer: mtimer::OneShot,
-    }
-    impl RticTask for StartSim {
+    struct Finish {}
+    impl RticTask for Finish {
         fn init() -> Self {
-            let mtimer = MTimer::instance().into_oneshot();
-            Self { mtimer }
+            Self {}
         }
         fn exec(&mut self) {
-            //Disable scoreboard
+            // Scoreboard disable
             mmio::write_u32(CFG_BASE_ADDR + CFG_TASK_OFFS, 0);
 
             let now = MTimer::instance().into_oneshot().duration().ticks();
-            let minstret = riscv::register::minstret::read64();
-            let mcycle = riscv::register::mcycle::read64();
+            let minstret = minstret::read64();
+            let mcycle = mcycle::read64();
 
             sprintln!(
                 "CPU util: {}%, instructions: {}",
