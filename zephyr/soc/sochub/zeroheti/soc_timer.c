e6b325cb3dd73d3fb3ac01859c89a30ba4a4fcb4
krabch <abdelkadir.chantar@tuni.fi>
2026-09-02 00:29:23 +0300

    sw-edf: make the mtime prescaler configurable

--- zephyr/soc/sochub/zeroheti/soc_timer.c
+++ zephyr/soc/sochub/zeroheti/soc_timer.c
@@ -1,11 +1,12 @@
 #include <zephyr/init.h>
 #include <zephyr/arch/cpu.h>
+#include <zephyr/sys/sys_io.h>
 
-static int start_mtimer(void) {
-	// Enable mtimer with prescaler 4
-	//sys_write32(0x401, 0x2110);
-	// Enable mtimer w/o prescaler
-	sys_write32(0x1, 0x3110);
+#define MTIMER_CTRL 0x3110U
+
+static int start_mtimer(void)
+{
+	sys_write32((CONFIG_ZEROHETI_MTIME_PRESCALER << 8) | 0x1U, MTIMER_CTRL);
 	return 0;
 }
 
