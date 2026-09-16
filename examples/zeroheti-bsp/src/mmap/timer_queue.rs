2e32468edcae6f0030d80dd51a7c2f6fe7653800
Antti Nurmi <antti.nurmi@tuni.fi>
2026-09-16 13:18:00 +0300

    Add timer queue test to workflow

--- examples/zeroheti-bsp/src/mmap/timer_queue.rs
+++ examples/zeroheti-bsp/src/mmap/timer_queue.rs
@@ -1,1 +1,6 @@
 pub const TQ_BASE: usize = 0x3800;
+pub const STAT_OFFS: usize = 0x0;
+pub const CTRL_OFFS: usize = 0x4;
+pub const REL_TS_OFFS: usize = 0xC;
+pub const ABS_TS_LO_OFFS: usize = 0x10;
+pub const ABS_TS_HI_OFFS: usize = 0x14;
