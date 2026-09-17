76402dddc184c83285bf5f147d4e1a292312835b
Antti Nurmi <antti.nurmi@tuni.fi>
2026-09-17 15:16:25 +0300

    Fix format_elf.sh script

---
+++ design_config.mk
@@ -0,0 +1,7 @@
+export DC_IMEM_BYTES = 65536
+export DC_NR_I_BANKS = 16
+export DC_DMEM_BYTES = 32768
+export DC_NR_D_BANKS = 8
+export DC_INTC       = EDFIC
+export DC_GIT_HASH   = $(shell git rev-parse --short HEAD)
+export DC_JTAG_ID    = feedc0d3
